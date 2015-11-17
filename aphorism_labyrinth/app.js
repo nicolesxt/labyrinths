aefacc4be330e4d945444c1845dd625f0e84db74
NicoSxt <123sxtnicole@gmail.com>
2015-11-17 14:10:25 -0500

    added one of the original sketches
    1.added one of the original sketches
    2.changed ports

--- aphorism_labyrinth/app.js
+++ aphorism_labyrinth/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var port= 8000;
-var url = "149.31.130.23"
+var port= 5009;
+var url = "192.168.1.251"
 var server = app.listen(port);
 app.use(express.static(__dirname + '/'));//serve diectory this file is in
 console.log('Simple static server listening at '+url+':'+port);
--- firstsketch/app.js
+++ firstsketch/app.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
-var port= 8000;
+var port= 7700;
 var url = "192.168.1.251"
 var server = app.listen(port);
 app.use(express.static(__dirname + '/'));//serve diectory this file is in
 console.log('Simple static server listening at '+url+':'+port);
-
-
-//sudo node app.js
